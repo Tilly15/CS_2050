809cb25a87977cba5a914a69f08c7185189d22ad
Tilly15 <jttilly14@gmail.com>
2025-02-08 13:44:36 -0700

    Create TillinghastGE01Polymorphism.java

---
+++ Eclipse Workspace/cs2050ClassWork/cs2050ClassWork/TillinghastGE01Polymorphism.java
@@ -0,0 +1,74 @@
+package cs2050ClassWork;
+
+public class TillinghastGE01Polymorphism {
+
+	public static void main(String[] args) {
+		Animal animal = new Animal("Bill","a",4,5,"b");
+		animal.eat();
+
+	}
+
+}
+
+class Animal{
+	private String name;
+	private String food;
+	private int weight;
+	private int sleep;
+	private String location;
+	
+	//constructor for super class Animal
+	public Animal(String name, String food, int weight,
+			int sleep, String location) {
+		this.name = name;
+		this.food = food;
+		this.weight = weight;
+		this.sleep = sleep;
+		this.location = location;
+	}
+	
+	//getters
+	public String getName() {
+		return name;
+	}
+	public String getFood() {
+		return food;
+	}
+	public double getWeight() {
+		return weight;
+	}
+	public double getSleep() {
+		return sleep;
+	}
+	public String getLocation() {
+		return location;
+	}
+	
+	//3 more public methods
+	public void eat() {
+		System.out.println(name + " is eating");
+	}
+	
+	
+}
+
+
+/**
+ * 
+
+class Bear extends Animal{
+	
+}
+
+class Elephant extends Animal{
+	
+}
+
+class Monkey extends Animal{
+	
+}
+
+class Sloth extends Animal{
+	
+}
+**/
