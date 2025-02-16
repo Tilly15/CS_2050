33f35bb7720b2e2a242a4acfff316603a63be54b
Tilly15 <jttilly14@gmail.com>
2025-02-15 17:50:05 -0700

    Update TillinghastFirstJavaProgram.java

--- Eclipse Workspace/cs2050ClassWork/TillinghastFirstJavaProgram.java
+++ Eclipse Workspace/cs2050ClassWork/TillinghastFirstJavaProgram.java
@@ -12,7 +12,7 @@ public class TillinghastFirstJavaProgram {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello");
+		System.out.print("Hello Java");
 
 	}
 
